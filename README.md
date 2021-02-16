e178405d0af89ab02e9d9ad812afcf0662dd417b
Shivam <jalotrashivam9@gmail.com>
2021-02-16 16:45:48 +0530

    Conda activate is now a step. Updated Readme

--- README.md
+++ README.md
@@ -7,7 +7,8 @@
 
 ## Setting Up the Development Arena 
 1. You must have Conda installed, since it provides containerisation we dont have to worry about building and running on Different Platforms/ OSes.
-2. First run "bash setup_env.sh" -> This will create a QUEUE_DETECTION conda environment.
+2. First run "bash setup_env.sh" -> This will create a `Queue_Detection` conda environment.
+3. Run `conda activate Queue_Detection` to activate the newly made env.
 2. Next run "bash setup.sh" -> This will install all the related dependencies.
 2. In case of error, read the *sh files and figure it out yourself.
 
--- setup.sh
+++ setup.sh
@@ -6,43 +6,41 @@
 # I am using Yolo from Pytorch Hub. 
 # See this : https://github.com/ultralytics/yolov5/issues/36
 
-# Step 1 
-ENV_NAME="QUEUE_DETECTION"
-conda init bash
-conda activate $ENV_NAME
-
-# Step 2 : Install Dependencies of UltraLytics 
+# Step 1 : Install Dependencies of UltraLytics 
 pip install -U opencv-python pillow pyYAML tqdm
+pip install -r requirements.txt
 
-# Step 3 : Install Pytorch 
+# Step 2 : Install Pytorch 
 function get_cuda_version(){
-    echo "Since you pressed 'Yes', what is the cuda version listed in /usr/local/cuda/version.txt ?"
-    echo "Options are '9.2' '10.1' '10.2' '11.0'"
-    select yn in "9.2" "10.1" "10.2" "11.0"; do 
-        9.2 ) conda install pytorch torchvision torchaudio cudatoolkit=9.2 -c pytorch;;
-        10.1 ) conda install pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch;;
-        10.2 ) conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch;;
-        11.0 ) conda install pytorch torchvision torchaudio cudatoolkit=11.0 -c pytorch;;
-        * ) echo "You fucked up !"; exit;; 
+    echo -e "Since you pressed 'Yes', what is the cuda version listed in /usr/local/cuda/version.txt ? \n"
+    echo -e "Options are '9.2' '10.1' '10.2' '11.0' \n"
+    while true; do
+    read -p "Type Yes or No, Make sure this is Case-Sensitive. " yn
+    case $yn in 
+        9.2 ) conda install pytorch torchvision torchaudio cudatoolkit=9.2 -c pytorch; break;;
+        10.1 ) conda install pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch; break;;
+        10.2 ) conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch; break;;
+        11.0 ) conda install pytorch torchvision torchaudio cudatoolkit=11.0 -c pytorch; break;;
+        * ) echo "You messed up !"; break;; 
     esac 
 done
 }
 
 
-
 function has_gpu () {
     echo "Do You have a GPU and more importantly CUDA installed." ? 
     echo "Run cat /usr/local/cuda/version.txt"
-    echo "Type Yes or No, Make sure this is Case-Sensitive"
-    select yn in "Yes" "No"; do
+    while true; do
+    read -p "Type Yes or No, Make sure this is Case-Sensitive. " yn
     case $yn in
-        Yes ) get_cuda_version();;
-        No ) conda install pytorch torchvision torchaudio cpuonly -c pytorch;;
+        [Yy]* ) get_cuda_version; break;;
+        [Nn]* ) conda install pytorch torchvision torchaudio cpuonly -c pytorch; break;;
+        * ) echo "Please answer yes or no.";;
     esac
 done
+
 }
 
-
-has_gpu()
+has_gpu
 
 
